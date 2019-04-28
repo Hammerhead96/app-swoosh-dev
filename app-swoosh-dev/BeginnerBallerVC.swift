a6146baed838a177932bca865e54754b97c352e7
Anthony Crane <anthonycrane@hotmail.com>
2019-04-28 00:27:27 -0700

    All 3 views laid out
    All three views are laid out. The VC's for them are created but no segue yet and only MainVC has any outlets so far.

---
+++ app-swoosh-dev/BeginnerBallerVC.swift
@@ -0,0 +1,30 @@
+//
+//  BeginnerBallerVC.swift
+//  app-swoosh-dev
+//
+//  Created by Mac User on 4/28/19.
+//  Copyright © 2019 Hammerhead96. All rights reserved.
+//
+
+import UIKit
+
+class BeginnerBallerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
